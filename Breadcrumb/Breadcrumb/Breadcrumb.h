0d48dee4c4bb1017c378ee7910d9f369134eaaca
Andr3wHur5t <Andr3wHur5t@live.com>
2015-02-04 18:45:32 -0800

    Added workspace, and project.

---
+++ Breadcrumb/Breadcrumb/Breadcrumb.h
@@ -0,0 +1,19 @@
+//
+//  Breadcrumb.h
+//  Breadcrumb
+//
+//  Created by Andrew Hurst on 2/4/15.
+//  Copyright (c) 2015 Breadcrumb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Breadcrumb.
+FOUNDATION_EXPORT double BreadcrumbVersionNumber;
+
+//! Project version string for Breadcrumb.
+FOUNDATION_EXPORT const unsigned char BreadcrumbVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Breadcrumb/PublicHeader.h>
+
+
